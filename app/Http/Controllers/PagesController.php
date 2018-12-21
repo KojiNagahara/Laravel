9c7c75dea3c9fc3013d633b9097425fbe7ab16b1
Koji Nagahara <kjnghr@gmail.com>
2018-12-21 13:55:39 +0900

    認証とAboutページを組み込んだ状態

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    /**
+     * アプリケーションのトップページを表示する。
+     *
+     * @return 最初の画面を表すViewオブジェクト
+     */
+    public function home() {
+        return view('welcome');
+    }
+
+    /**
+     * アプリケーションの説明を記述したページを表示する。
+     *
+     * @return 説明画面を表すViewオブジェクト
+     */
+    public function about()
+    {
+        return view('about');
+    }
+
+}
